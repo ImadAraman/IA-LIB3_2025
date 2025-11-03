55407bde15d20dfe8f244f74a86aeeea2e40feb3
ImadAraman <s12240486@stu.najah.edu>
2025-11-03 13:53:29 +0200

    Sprint 1: Add Book domain entity and unit tests

---
+++ src/main/java/edu/najah/library/domain/Book.java
@@ -0,0 +1,167 @@
+package edu.najah.library.domain;
+
+import java.util.Objects;
+
+/**
+ * Represents a book in the library management system.
+ * Each book has a title, author, ISBN, and availability status.
+ * 
+ * @author Imad Araman
+ * @version 1.0
+ */
+public class Book {
+    
+    private String title;
+    private String author;
+    private String isbn;
+    private boolean isAvailable;
+    
+    /**
+     * Default constructor.
+     */
+    public Book() {
+        this.isAvailable = true; // New books are available by default
+    }
+    
+    /**
+     * Constructs a Book with specified details.
+     * Book is available by default.
+     * 
+     * @param title the book's title
+     * @param author the book's author
+     * @param isbn the book's ISBN
+     */
+    public Book(String title, String author, String isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = true;
+    }
+    
+    /**
+     * Constructs a Book with specified details and availability.
+     * 
+     * @param title the book's title
+     * @param author the book's author
+     * @param isbn the book's ISBN
+     * @param isAvailable the book's availability status
+     */
+    public Book(String title, String author, String isbn, boolean isAvailable) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+    
+    /**
+     * Gets the book's title.
+     * 
+     * @return the title
+     */
+    public String getTitle() {
+        return title;
+    }
+    
+    /**
+     * Sets the book's title.
+     * 
+     * @param title the title to set
+     */
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    
+    /**
+     * Gets the book's author.
+     * 
+     * @return the author
+     */
+    public String getAuthor() {
+        return author;
+    }
+    
+    /**
+     * Sets the book's author.
+     * 
+     * @param author the author to set
+     */
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+    
+    /**
+     * Gets the book's ISBN.
+     * 
+     * @return the ISBN
+     */
+    public String getIsbn() {
+        return isbn;
+    }
+    
+    /**
+     * Sets the book's ISBN.
+     * 
+     * @param isbn the ISBN to set
+     */
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+    
+    /**
+     * Checks if the book is available for borrowing.
+     * 
+     * @return true if available, false otherwise
+     */
+    public boolean isAvailable() {
+        return isAvailable;
+    }
+    
+    /**
+     * Sets the book's availability status.
+     * 
+     * @param available the availability status to set
+     */
+    public void setAvailable(boolean available) {
+        this.isAvailable = available;
+    }
+    
+    /**
+     * Compares this book with another object for equality.
+     * Two books are equal if they have the same ISBN.
+     * 
+     * @param obj the object to compare with
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Book book = (Book) obj;
+        return Objects.equals(isbn, book.isbn);
+    }
+    
+    /**
+     * Generates a hash code for this book.
+     * 
+     * @return the hash code
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(isbn);
+    }
+    
+    /**
+     * Returns a string representation of this book.
+     * 
+     * @return string representation
+     */
+    @Override
+    public String toString() {
+        return "Book{" +
+                "title='" + title + '\'' +
+                ", author='" + author + '\'' +
+                ", isbn='" + isbn + '\'' +
+                ", isAvailable=" + isAvailable +
+                '}';
+    }
+}
---
+++ src/test/java/edu/najah/library/domain/BookTest.java
@@ -0,0 +1,100 @@
+package edu.najah.library.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the Book entity.
+ * 
+ * @author Imad Araman
+ * @version 1.0
+ */
+public class BookTest {
+    
+    /**
+     * Test creating a book with constructor.
+     */
+    @Test
+    public void testBookCreation() {
+        Book book = new Book("Clean Code", "Robert Martin", "978-0132350884");
+        
+        assertEquals("Clean Code", book.getTitle());
+        assertEquals("Robert Martin", book.getAuthor());
+        assertEquals("978-0132350884", book.getIsbn());
+        assertTrue(book.isAvailable(), "New books should be available by default");
+    }
+    
+    /**
+     * Test book creation with availability parameter.
+     */
+    @Test
+    public void testBookCreationWithAvailability() {
+        Book book = new Book("Java Programming", "John Doe", "123456789", false);
+        
+        assertFalse(book.isAvailable(), "Book should be unavailable as specified");
+    }
+    
+    /**
+     * Test book setters.
+     */
+    @Test
+    public void testBookSetters() {
+        Book book = new Book();
+        book.setTitle("Design Patterns");
+        book.setAuthor("Gang of Four");
+        book.setIsbn("978-0201633610");
+        book.setAvailable(false);
+        
+        assertEquals("Design Patterns", book.getTitle());
+        assertEquals("Gang of Four", book.getAuthor());
+        assertEquals("978-0201633610", book.getIsbn());
+        assertFalse(book.isAvailable());
+    }
+    
+    /**
+     * Test book equality based on ISBN.
+     */
+    @Test
+    public void testBookEquality() {
+        Book book1 = new Book("Title1", "Author1", "12345");
+        Book book2 = new Book("Title2", "Author2", "12345");
+        Book book3 = new Book("Title1", "Author1", "67890");
+        
+        assertEquals(book1, book2, "Books with same ISBN should be equal");
+        assertNotEquals(book1, book3, "Books with different ISBN should not be equal");
+    }
+    
+    /**
+     * Test book hash code consistency.
+     */
+    @Test
+    public void testBookHashCode() {
+        Book book1 = new Book("Title1", "Author1", "12345");
+        Book book2 = new Book("Different Title", "Different Author", "12345");
+        
+        assertEquals(book1.hashCode(), book2.hashCode(), 
+                "Books with same ISBN should have same hash code");
+    }
+    
+    /**
+     * Test book toString method.
+     */
+    @Test
+    public void testBookToString() {
+        Book book = new Book("Clean Code", "Robert Martin", "978-0132350884");
+        String result = book.toString();
+        
+        assertTrue(result.contains("Clean Code"), "toString should contain title");
+        assertTrue(result.contains("Robert Martin"), "toString should contain author");
+        assertTrue(result.contains("978-0132350884"), "toString should contain ISBN");
+    }
+    
+    /**
+     * Test default constructor creates available book.
+     */
+    @Test
+    public void testDefaultConstructor() {
+        Book book = new Book();
+        assertTrue(book.isAvailable(), "Default constructor should create available book");
+    }
+}
