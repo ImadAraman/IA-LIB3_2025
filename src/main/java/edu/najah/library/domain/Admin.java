81f33c1b72ce3510794f2ee4d0e0bac1c8287dcb
ImadAraman <s12240486@stu.najah.edu>
2025-11-03 13:46:32 +0200

    Sprint 1: Add Admin domain entity and unit tests

---
+++ src/main/java/edu/najah/library/domain/Admin.java
@@ -0,0 +1,104 @@
+package edu.najah.library.domain;
+
+import java.util.Objects;
+
+/**
+ * Represents an administrator in the library management system.
+ * Administrators can log in to manage books and users.
+ * 
+ * @author Imad Araman
+ * @version 1.0
+ */
+public class Admin {
+    
+    private String username;
+    private String password;
+    
+    /**
+     * Default constructor.
+     */
+    public Admin() {
+    }
+    
+    /**
+     * Constructs an Admin with specified username and password.
+     * 
+     * @param username the administrator's username
+     * @param password the administrator's password
+     */
+    public Admin(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+    
+    /**
+     * Gets the administrator's username.
+     * 
+     * @return the username
+     */
+    public String getUsername() {
+        return username;
+    }
+    
+    /**
+     * Sets the administrator's username.
+     * 
+     * @param username the username to set
+     */
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    /**
+     * Gets the administrator's password.
+     * 
+     * @return the password
+     */
+    public String getPassword() {
+        return password;
+    }
+    
+    /**
+     * Sets the administrator's password.
+     * 
+     * @param password the password to set
+     */
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    /**
+     * Compares this admin with another object for equality.
+     * Two admins are equal if they have the same username.
+     * 
+     * @param obj the object to compare with
+     * @return true if equal, false otherwise
+     */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Admin admin = (Admin) obj;
+        return Objects.equals(username, admin.username);
+    }
+    
+    /**
+     * Generates a hash code for this admin.
+     * 
+     * @return the hash code
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(username);
+    }
+    
+    /**
+     * Returns a string representation of this admin.
+     * 
+     * @return string representation
+     */
+    @Override
+    public String toString() {
+        return "Admin{username='" + username + "'}";
+    }
+}
---
+++ src/test/java/edu/najah/library/domain/AdminTest.java
@@ -0,0 +1,74 @@
+package edu.najah.library.domain;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Unit tests for the Admin entity.
+ * 
+ * @author Imad Araman
+ * @version 1.0
+ */
+public class AdminTest {
+    
+    /**
+     * Test creating an admin with constructor.
+     */
+    @Test
+    public void testAdminCreation() {
+        Admin admin = new Admin("admin", "password123");
+        
+        assertEquals("admin", admin.getUsername());
+        assertEquals("password123", admin.getPassword());
+    }
+    
+    /**
+     * Test admin setters.
+     */
+    @Test
+    public void testAdminSetters() {
+        Admin admin = new Admin();
+        admin.setUsername("newadmin");
+        admin.setPassword("newpass");
+        
+        assertEquals("newadmin", admin.getUsername());
+        assertEquals("newpass", admin.getPassword());
+    }
+    
+    /**
+     * Test admin equality based on username.
+     */
+    @Test
+    public void testAdminEquality() {
+        Admin admin1 = new Admin("admin", "pass1");
+        Admin admin2 = new Admin("admin", "pass2");
+        Admin admin3 = new Admin("different", "pass1");
+        
+        assertEquals(admin1, admin2, "Admins with same username should be equal");
+        assertNotEquals(admin1, admin3, "Admins with different usernames should not be equal");
+    }
+    
+    /**
+     * Test admin hash code consistency.
+     */
+    @Test
+    public void testAdminHashCode() {
+        Admin admin1 = new Admin("admin", "pass");
+        Admin admin2 = new Admin("admin", "differentpass");
+        
+        assertEquals(admin1.hashCode(), admin2.hashCode(), 
+                "Admins with same username should have same hash code");
+    }
+    
+    /**
+     * Test admin toString method.
+     */
+    @Test
+    public void testAdminToString() {
+        Admin admin = new Admin("admin", "password");
+        String result = admin.toString();
+        
+        assertTrue(result.contains("admin"), "toString should contain username");
+        assertFalse(result.contains("password"), "toString should not contain password");
+    }
+}
